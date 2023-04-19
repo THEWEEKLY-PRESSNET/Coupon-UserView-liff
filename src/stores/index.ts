94ff316f5b20b5ceb7258009b9bbd63bbe9a81eb
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-04-19 13:18:48 +0900

    update slice
    add modal

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,17 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
+import modal, { Modal } from "./modal";
 import topState, { TopState } from "./topState";
 
 export type Root = {
   topState: TopState;
+  modal: Modal;
 };
 
 const reducer = combineReducers({
   topState,
+  modal,
 });
 
 const store = configureStore({ reducer });
