a24697f0512aa44fda7765bbfa1d41bc45441389
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-04-23 18:37:22 +0900

    pass using global slice

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,18 +6,18 @@
 
 // You can delete this file if you're not using it
 
-const React = require("react")
-const { Provider } = require("react-redux")
-const store = require("./src/stores")
+// const React = require("react")
+// const { Provider } = require("react-redux")
+// const store = require("./src/stores")
 // console.log("brouser stores", store)
 
-exports.wrapRootElement = ({ element }) => {
-    return (
-        <Provider store={store.default}>
-            {element}
-        </Provider>
-    )
-}
+// exports.wrapRootElement = ({ element }) => {
+//     return (
+//         <Provider store={store.default}>
+//             {element}
+//         </Provider>
+//     )
+// }
 
 const { createRoot } = require("react-dom/client")
 const liff = require("@line/liff")
