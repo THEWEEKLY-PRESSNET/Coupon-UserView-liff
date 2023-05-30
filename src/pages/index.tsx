3348e3493734e82978e56e8aea3ef0e5f7060774
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-05-30 20:51:49 +0900

    milestone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { Box } from "@mui/material";
 
 import Seo from "../components/seo";
+import Question from "../Question";
 import Lotto from "../Lotto";
+
 import { usePreRender } from "../hooks/usePreRender";
+import { serchQueryToOgj } from "../utils/handlePath";
 import store from "../stores";
+import { Link, PageProps, navigate } from "gatsby";
 
 const PreRender: React.FC = () => <div>loading...</div>;
 
-const IndexPage: React.FC = ({ location }) => {
+const IndexPage: React.FC<PageProps> = ({ location }) => {
   console.log("location", location);
-  return (
+  console.log("state", serchQueryToOgj(location.search));
+  const { state, hash } = serchQueryToOgj(location.search);
+  useEffect(() => {
+    if (!state && !hash) {
+      // window.location.href = "https://www.higashihiroshima-digital.com/";
+      // navigate("/404");
+      console.log("");
+    }
+  }, [state, hash]);
+
+  return usePreRender(
+    PreRender,
     <Provider store={store}>
-      <Lotto />
+      {state !== undefined && <Question />}
+      {hash === undefined && <Lotto />}
     </Provider>
   );
 };
