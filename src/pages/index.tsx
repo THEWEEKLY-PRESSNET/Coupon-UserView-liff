0242fe51dcf6e2afcf451edfb0e62157e372f83a
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-05-30 18:08:26 +0900

    milestone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,19 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import { Box } from "@mui/material";
 
 import Seo from "../components/seo";
-import App from "../App";
+import Lotto from "../Lotto";
 import { usePreRender } from "../hooks/usePreRender";
 import store from "../stores";
 
 const PreRender: React.FC = () => <div>loading...</div>;
 
-const IndexPage: React.FC = () => {
+const IndexPage: React.FC = ({ location }) => {
+  console.log("location", location);
   return (
     <Provider store={store}>
-      <App />
+      <Lotto />
     </Provider>
   );
 };
-
-const test = () => {
-  const topState = useSelector((s: Root) => s.topState);
-  console.log("topstate", topState);
-
-  return <Box>hello</Box>;
-};
-
-export const Head = () => (
-  <Seo title="Home" description="" children={undefined} />
-);
 
 export default IndexPage;
