d71d979818a1990dc7d28291a2d6189dd8981c66
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-06-21 15:00:57 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,32 +11,36 @@ const homeUrl = "https://www.higashihiroshima-digital.com/";
 
 const PreRender: React.FC = () => <div>loading...</div>;
 
-const IndexPage: React.FC<PageProps> = ({ location }) => {
-  console.log("location", location);
+const After: React.FC = () => {
   const { error, isLoggedIn, isReady } = useLiff();
   // console.log("liff", liff);
   console.log("isLoggedIn", isLoggedIn);
   console.log("isReady", isReady);
 
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     liff
-  //       .init({ liffId: "1660941787-3qkMjKae" })
-  //       .then(() => {
-  //         // const root = createRoot(container);
-  //         // root.render(element);
-  //         console.log("init success");
-  //       })
-  //       .catch(e => {
-  //         alert(`LIFF error: ${e.message}`);
-  //       });
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      liff
+        .init({ liffId: "1660941787-3qkMjKae" })
+        .then(() => {
+          // const root = createRoot(container);
+          // root.render(element);
+          console.log("init success");
+        })
+        .catch(e => {
+          alert(`LIFF error: ${e.message}`);
+        });
+    }
+  }, []);
+  return <div>INDEX!!!!</div>;
+};
+
+const IndexPage: React.FC<PageProps> = ({ location }) => {
+  console.log("location", location);
 
   return usePreRender(
     PreRender,
     <Provider store={store}>
-      <div>index</div>
+      <After />
     </Provider>
   );
 };
