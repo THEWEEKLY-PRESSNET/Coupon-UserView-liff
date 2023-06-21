406cdad0423a38bcb7fd2ebbffdf7fb50c8a02eb
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-06-21 14:32:21 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
-import { liff } from "@line/liff";
+// import { liff } from "@line/liff";
 import { useLiff } from "react-liff";
 
 import { usePreRender } from "../hooks/usePreRender";
@@ -13,22 +13,25 @@ const PreRender: React.FC = () => <div>loading...</div>;
 
 const IndexPage: React.FC<PageProps> = ({ location }) => {
   console.log("location", location);
-  const { error, isLoggedIn, isReady } = useLiff();
+  const { error, isLoggedIn, isReady, liff } = useLiff();
   // console.log("liff", liff);
   console.log("isLoggedIn", isLoggedIn);
+  console.log("isReady", isReady);
 
-  // useEffect(() => {
-  //   liff
-  //     .init({ liffId: "1660941787-3qkMjKae" })
-  //     .then(() => {
-  //       // const root = createRoot(container);
-  //       // root.render(element);
-  //       console.log("init success");
-  //     })
-  //     .catch(e => {
-  //       alert(`LIFF error: ${e.message}`);
-  //     });
-  // }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      liff
+        .init({ liffId: "1660941787-3qkMjKae" })
+        .then(() => {
+          // const root = createRoot(container);
+          // root.render(element);
+          console.log("init success");
+        })
+        .catch(e => {
+          alert(`LIFF error: ${e.message}`);
+        });
+    }
+  }, []);
 
   return usePreRender(
     PreRender,
