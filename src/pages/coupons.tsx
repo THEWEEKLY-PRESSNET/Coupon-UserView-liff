f2deaed6427c51e44ce5e1db0b946a3157104a81
ikkikki <50198675+ikkikki@users.noreply.github.com>
2023-06-22 13:54:32 +0900

    Fix Coupons

--- src/pages/coupons.tsx
+++ src/pages/coupons.tsx
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from "react";
-// import { navigate } from "gatsby";
 import { Provider, useDispatch } from "react-redux";
-import { LiffProvider, useLiff } from "react-liff";
-import { ThemeProvider } from "@mui/material/styles";
 
+import Loading from "../components/Loading";
 import Coupons from "../Coupons";
 import { usePreRender } from "../hooks/usePreRender";
 import store from "../stores";
-import theme from "../styles/theme";
 import type { PageProps } from "gatsby";
 import { updateCouponState } from "../stores/couponState";
-
-const homeUrl = "https://www.higashihiroshima-digital.com/";
-const liffId = "1661486792-mWQ6Adxo";
 
 const PreRender: React.FC = () => <div>loading...</div>;
 
 const After: React.FC = () => {
-  //   const [displayName, setDisplayName] = useState("");
-  //   const [profile, setProfile] = useState({});
   const [friendship, setFriend] = useState(false);
   const { error, isLoggedIn, isReady, liff } = useLiff();
   console.log("liff id", liff);
@@ -27,12 +19,10 @@ const After: React.FC = () => {
   //   console.log("friendship", friendship);
   const dispatch = useDispatch();
   useEffect(() => {
-    if (!isLoggedIn) return;
-    // (async () => {
-    //   const profile = await liff.getProfile();
-    //   setDisplayName(profile.displayName);
-    //   console.log("profile", profile);
-    // })();
+    if (!isLoggedIn) {
+      liff.login();
+    }
+
     (async () => {
       const friendship = await liff.getFriendship();
       console.log("friendship useEffect", friendship);
@@ -40,11 +30,9 @@ const After: React.FC = () => {
       if (!friendship.friendFlag) {
         window.location.replace("https://lin.ee/LWvjdI0");
       }
-      //   setFriend(friendship.frienFlag);
     })();
     (async () => {
       const idToken = liff.getIDToken();
-      // setFriend(friendship.frienFlag);
       console.log("idToken", idToken);
       dispatch(updateCouponState({ idToken }));
     })();
@@ -53,46 +41,27 @@ const After: React.FC = () => {
   const showDisplayName = () => {
     if (error) return <p>Something is wrong.</p>;
     if (!isReady) return <p>Loading...</p>;
-    if (isReady && !isLoggedIn) {
-      liff.login();
+    if (!isLoggedIn) {
+      return (
+        <button className="App-button" onClick={liff.login}>
+          Login
+        </button>
+      );
     }
-    // if (!isLoggedIn) {
-    //   //   liff.login();
-    //   return (
-    //     <button className="App-button" onClick={liff.login}>
-    //       Login
-    //     </button>
-    //   );
-    // }
-    // if (!friendship) return <p>公式アカウントを友達追加してください</p>;
-
+    if (!friendship) return <p>公式アカウントを友達追加してください</p>;
     return <Coupons />;
-    // return (
-    //   //   <Provider store={store}>
-    //   <ThemeProvider theme={theme}>
-    //     <Coupons />
-    //     <p>Welcome to the react-liff demo app, {displayName}!</p>
-    //     <p>state: {liff.state}</p>
-    //     <p>friendship: {friendship}</p>
-    //     <p></p>
-    //     <button className="App-button" onClick={liff.logout}>
-    //       Logout
-    //     </button>
-    //   </ThemeProvider>
-    //   //   {/* </Provider> */}
-    // );
   };
   return (
     <>
-      {(friendship && <>{showDisplayName()}</>) || (
-        <>
-          <p>公式アカウントを友達追加してください</p>
-          <a href="https://lin.ee/LWvjdI0">東広島デジタル公式アカウント</a>
-        </>
-      )}
+      {(!isReady && <div>loading...</div>) ||
+        (friendship && <>{showDisplayName()}</>) || (
+          <>
+            <p>公式アカウントを友達追加してください</p>
+            <a href="https://lin.ee/LWvjdI0">東広島デジタル公式アカウント</a>
+          </>
+        )}
     </>
   );
-  //   return <Coupons />;
 };
 
 const CouponsPage: React.FC<PageProps> = ({ location }) => {
